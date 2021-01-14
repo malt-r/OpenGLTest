28406ee193c7092415d62f2baaf75e06150396c0
Malt_R <malte.r@mail.de>
2021-01-14 19:30:09 +0100

    added model and view matrix

--- OpenGLTest/src/Application.cpp
+++ OpenGLTest/src/Application.cpp
@@ -109,10 +109,23 @@ int main(void)
         // the matrix itself defines the bounbaries for the projection
         glm::mat4 proj = glm::ortho(0.f, 960.f, 0.f, 540.f, -1.0f, 1.0f);
 
+        // 'move' the camera 100 units to the right
+        // because there is no such thing as a camera, we need to move everything
+        // else 100 units to the left to create the illusion of moving a camera
+        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+
+        // move 'model' more to th center of the screen --> translate 200 units up and right
+        glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
+
+        // column-major --> multiplication from right to left to create the result we want
+        // which is first apply the view matrix, then the projection matrix
+        glm::mat4 mvp = proj * view * model;
+
+
         Shader shader("res/shader/Basic.shader");
         shader.Bind();
         shader.SetUniform4f("u_Color", 0.5f, 0.0f, 0.0f, 1.0f);
-        shader.SetUniformMat4f("u_MVP", proj);
+        shader.SetUniformMat4f("u_MVP", mvp);
 
         float r = 0.0f;
         float rIncrement = 0.05f;
